ebd2db3619ab4282575a573804d6c497a06f59e3
Mario Rezende <mariorez@gmail.com>
2020-12-20 17:04:44 -0300

    add music and sound affects

--- core/src/main/java/org/seariver/BaseScreen.java
+++ core/src/main/java/org/seariver/BaseScreen.java
@@ -5,11 +5,13 @@ import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.scenes.scene2d.Event;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputEvent.Type;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 
 public abstract class BaseScreen implements Screen, InputProcessor {
-
     protected Stage mainStage;
     protected Stage uiStage;
     protected Table uiTable;
@@ -84,6 +86,13 @@ public abstract class BaseScreen implements Screen, InputProcessor {
         im.removeProcessor(this);
         im.removeProcessor(uiStage);
         im.removeProcessor(mainStage);
+    }
+
+    /**
+     * Useful for checking for touch-down events.
+     */
+    public boolean isTouchDownEvent(Event e) {
+        return (e instanceof InputEvent) && ((InputEvent) e).getType().equals(Type.touchDown);
     }
 
     // methods required by InputProcessor interface
--- core/src/main/java/org/seariver/screen/LevelScreen.java
+++ core/src/main/java/org/seariver/screen/LevelScreen.java
@@ -1,10 +1,12 @@
 package org.seariver.screen;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Event;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.Button.ButtonStyle;
@@ -21,19 +23,20 @@ import org.seariver.actor.Turtle;
 import org.seariver.actor.Whirlpool;
 import org.seariver.actor.ui.DialogBox;
 
-import static com.badlogic.gdx.graphics.Color.BROWN;
-import static com.badlogic.gdx.graphics.Color.CYAN;
-import static com.badlogic.gdx.graphics.Color.TAN;
-import static com.badlogic.gdx.scenes.scene2d.InputEvent.Type.touchDown;
-
 public class LevelScreen extends BaseScreen {
-
     private Turtle turtle;
     private boolean win;
     private Label starfishLabel;
+
     private DialogBox dialogBox;
 
+    private float audioVolume;
+    private Sound waterDrop;
+    private Music instrumental;
+    private Music oceanSurf;
+
     public void initialize() {
+
         BaseActor ocean = new BaseActor(0, 0, mainStage);
         ocean.loadTexture("water-border.jpg");
         ocean.setSize(1200, 900);
@@ -62,7 +65,7 @@ public class LevelScreen extends BaseScreen {
         // user interface code
 
         starfishLabel = new Label("Starfish Left:", BaseGame.labelStyle);
-        starfishLabel.setColor(CYAN);
+        starfishLabel.setColor(Color.CYAN);
 
         ButtonStyle buttonStyle = new ButtonStyle();
 
@@ -71,31 +74,69 @@ public class LevelScreen extends BaseScreen {
         buttonStyle.up = new TextureRegionDrawable(buttonRegion);
 
         Button restartButton = new Button(buttonStyle);
-        restartButton.setColor(CYAN);
+        restartButton.setColor(Color.CYAN);
+
         restartButton.addListener((Event e) -> {
-            if (!(e instanceof InputEvent) || !((InputEvent) e).getType().equals(touchDown)) return false;
+            if (!isTouchDownEvent(e)) return false;
+
+            instrumental.dispose();
+            oceanSurf.dispose();
+
             StarfishGame.setActiveScreen(new LevelScreen());
-            return false;
+            return true;
+        });
+
+        ButtonStyle buttonStyle2 = new ButtonStyle();
+
+        Texture buttonTex2 = new Texture(Gdx.files.internal("audio.png"));
+        TextureRegion buttonRegion2 = new TextureRegion(buttonTex2);
+        buttonStyle2.up = new TextureRegionDrawable(buttonRegion2);
+
+        Button muteButton = new Button(buttonStyle2);
+        muteButton.setColor(Color.CYAN);
+
+        muteButton.addListener((Event e) -> {
+            if (!isTouchDownEvent(e)) return false;
+
+            audioVolume = 1 - audioVolume;
+            instrumental.setVolume(audioVolume);
+            oceanSurf.setVolume(audioVolume);
+
+            return true;
         });
 
         uiTable.pad(10);
         uiTable.add(starfishLabel).top();
         uiTable.add().expandX().expandY();
+        uiTable.add(muteButton).top();
         uiTable.add(restartButton).top();
 
         dialogBox = new DialogBox(0, 0, uiStage);
-        dialogBox.setBackgroundColor(TAN);
-        dialogBox.setFontColor(BROWN);
+        dialogBox.setBackgroundColor(Color.TAN);
+        dialogBox.setFontColor(Color.BROWN);
         dialogBox.setDialogSize(600, 100);
         dialogBox.setFontScale(0.80f);
         dialogBox.alignCenter();
         dialogBox.setVisible(false);
 
         uiTable.row();
-        uiTable.add(dialogBox).colspan(3);
+        uiTable.add(dialogBox).colspan(4);
+
+        waterDrop = Gdx.audio.newSound(Gdx.files.internal("Water_Drop.ogg"));
+        instrumental = Gdx.audio.newMusic(Gdx.files.internal("Master_of_the_Feast.ogg"));
+        oceanSurf = Gdx.audio.newMusic(Gdx.files.internal("Ocean_Waves.ogg"));
+
+        audioVolume = 1.00f;
+        instrumental.setLooping(true);
+        instrumental.setVolume(audioVolume);
+        instrumental.play();
+        oceanSurf.setLooping(true);
+        oceanSurf.setVolume(audioVolume);
+        oceanSurf.play();
     }
 
     public void update(float dt) {
+
         for (BaseActor rockActor : BaseActor.getList(mainStage, "org.seariver.actor.Rock"))
             turtle.preventOverlap(rockActor);
 
@@ -103,6 +144,7 @@ public class LevelScreen extends BaseScreen {
             Starfish starfish = (Starfish) starfishActor;
             if (turtle.overlaps(starfish) && !starfish.collected) {
                 starfish.collected = true;
+                waterDrop.play(audioVolume);
                 starfish.clearActions();
                 starfish.addAction(Actions.fadeOut(1));
                 starfish.addAction(Actions.after(Actions.removeActor()));
