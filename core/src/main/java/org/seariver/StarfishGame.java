c9993e61050cce2e243fb63da28972cc6ace511c
Mario Rezende <mariorez@gmail.com>
2021-01-20 21:35:34 -0300

    implement TilemapActor to parse tile map file

--- core/src/main/java/org/seariver/StarfishGame.java
+++ core/src/main/java/org/seariver/StarfishGame.java
@@ -1,11 +1,11 @@
 package org.seariver;
 
-import org.seariver.screen.MenuScreen;
+import org.seariver.screen.LevelScreen;
 
 public class StarfishGame extends BaseGame {
 
     public void create() {
         super.create();
-        setActiveScreen(new MenuScreen());
+        setActiveScreen(new LevelScreen());
     }
 }
